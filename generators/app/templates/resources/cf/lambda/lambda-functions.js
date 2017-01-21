7179fb462542457898df0d42ce6224798946a064
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2017-01-21 00:01:21 -0500

    Moved environment definition to top of module

--- generators/app/templates/resources/cf/lambda/lambda-functions.js
+++ generators/app/templates/resources/cf/lambda/lambda-functions.js
@@ -7,6 +7,8 @@ const AliasTemplate = LambdaTemplates.AliasTemplate;
 const _lambdaConfig = require('../../../src/lambda-config');
 
 module.exports = (dirInfo) => {
+    const environments = [<%- projectTargetEnvironments.map(item => `'${item}'`).join(',') %>];
+
     return _lambdaConfig.lambdas.map((lambda) => {
         const roleName = '<%= projectPrefix %>.default_lambda_role';
         const roleKey = dirInfo.getNamespacedToken('iam_role', roleName);
@@ -31,7 +33,7 @@ module.exports = (dirInfo) => {
             functionTemplate.setTimeout(lambda.timeout);
         }
 
-        const aliasTemplates = [<%- projectTargetEnvironments.map(item => `'${item}'`).join(',') %>].map((envName) => {
+        const aliasTemplates = environments.map((envName) => {
             const aliasKey = dirInfo.getNamespacedToken('lambda_alias', `${functionName}_${envName}`);
             return new AliasTemplate(aliasKey, envName, functionName)
                 .addDependency(functionKey);
